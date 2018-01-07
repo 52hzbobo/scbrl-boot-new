4b9399d524b3e16c92ad212e9db461dd565a194a
52hzbobo <15959276686@139.com>
2018-01-07 13:24:24 +0800

    By

--- scbrl-common/src/main/java/com/scbrl/util/JsonUtil.java
+++ scbrl-common/src/main/java/com/scbrl/util/JsonUtil.java
@@ -3,6 +3,10 @@ package com.scbrl.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Bruce.Liu
@@ -10,10 +14,30 @@ import com.google.gson.GsonBuilder;
  */
 public class JsonUtil {
 
+    /**
+     * 对象转Json字符串
+     * @param object
+     * @return
+     */
     public static String toJson(Object object) {
         GsonBuilder gsonBuilder = new GsonBuilder().serializeNulls();
         gsonBuilder.setPrettyPrinting();
         Gson gson = gsonBuilder.create();
         return gson.toJson(object);
+    }
+
+    /**
+     * 字符串转Map
+     * @param json
+     * @return
+     */
+    public static Map<String,Object> toMap(String json){
+        try{
+            Map<String,Object> map = new Gson().fromJson(json, new TypeToken<HashMap<String,Object>>(){}.getType());
+            return map;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
